20567b426ca59b5cc82b906e1afaa1f945a8eadc
alekandelav <140648372+AleksandrLAV@users.noreply.github.com>
2023-08-20 14:30:12 +0300

    commit

--- seminar/Task00/Program.cs
+++ seminar/Task00/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Internal;
+using System;
+using Microsoft.VisualBasic.CompilerServices;
+// Напишите программу, которая
+// 1. На выход принимает число и
+// 2. Выдает его квадрат (число умножение на само себя).
+
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+Console.WriteLine ("Введите цело число");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+Console.WriteLine($"Квадрат числа {number} = {square}");
